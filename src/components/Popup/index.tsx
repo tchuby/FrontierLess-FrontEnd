6e794e822c268caf2e0aa2038729859895c7b26c
LUAN HENRIQUE GROSSERT <luan_grossert@estudante.sc.senai.br>
2024-09-27 20:57:46 -0300

    create popup Confirt

---
+++ src/components/Popup/index.tsx
@@ -0,0 +1,40 @@
+interface PopupProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+export default function Popup({ isOpen, onClose, onConfirm }: PopupProps) {
+    if (!isOpen) return null; // Não renderiza se não estiver aberto
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="relative p-4 w-full max-w-md">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button 
+                        type="button" 
+                        onClick={onClose} 
+                        className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center">
+                    </button>
+                    <div className="p-4 md:p-5 text-center">
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Tem certeza que deseja deletar este projeto? Todas as informações serão perdidas.
+                        </h3>
+                        <button 
+                            onClick={onConfirm} 
+                            type="button" 
+                            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                            Sim, tenho certeza
+                        </button>
+                        <button 
+                            onClick={onClose} 
+                            type="button" 
+                            className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:outline-none">
+                            Não, cancele
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -3,6 +3,7 @@ import Select from '../Select';
 import Accordion from "./components/Accordion";
 import H2 from '../H2';
 import Comments from '../Comments';
+import Popup from '../Popup';
 
 type Etapa = {
     key: number;
@@ -18,7 +19,7 @@ interface Props {
 }
 
 let cont = 0;
-export default function Project({ project, onDelete}: Props) {
+export default function Project({ project, onDelete }: Props) {
     const [etapas, setEtapas] = useState<Etapa[]>([]);
     const addEtapa = () => {
         const newEtapa: Etapa = {
@@ -32,8 +33,22 @@ export default function Project({ project, onDelete}: Props) {
     };
 
     const handleDeleteEtapa = (key: number) => {
-        const updatedEtapas = etapas.filter((etapa) => etapa.key !== key); // Remove a etapa pela chave
-        setEtapas(updatedEtapas); // Atualiza o estado com as etapas restantes
+        const updatedEtapas = etapas.filter((etapa) => etapa.key !== key);
+        setEtapas(updatedEtapas);
+    };
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const handleDeleteClick = () => {
+        setIsPopupOpen(true);
+    };
+
+    const handleClosePopup = () => {
+        setIsPopupOpen(false);
+    };
+
+    const handleConfirmDelete = () => {
+        onDelete(project.key);
+        setIsPopupOpen(false);
     };
 
     return (
@@ -59,29 +74,38 @@ export default function Project({ project, onDelete}: Props) {
                         <option value="pós-graduação">Pós-graduação</option>
                         <option value="pesquisa">Pesquisa</option>
                     </Select>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Custo total</label>
+                        <h2 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">R$ 200.000,00</h2>
+                    </div>
                 </div>
             </div>
 
             <div>
                 <div className="w-full text-center mb-4">
-                    <button type="button" onClick={addEtapa} title="Adicionar Etapa" className="mt-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">+ Etapas</button>
+                    <button type="button" onClick={addEtapa} title="Adicionar Etapa" className="mt-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:text-blue-900">+ Etapas</button>
                 </div>
                 {etapas.map((etapa) => (
                     <Accordion key={etapa.key} etapa={etapa} onDelete={handleDeleteEtapa} />
                 ))}
-
             </div>
 
-            <div className="mt-6 flex items-center justify-between  border-b border-gray-900/10 pb-12">
-                <button type="button" onClick={() => onDelete(project.key)} className="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">Deletar</button>
-                <div>
-                    <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l-lg">Cancelar</button>
-                    <button type="submit" className="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded-r-lg">Salvar</button>
+            <div className="mt-4 flex flex-col md:flex-row items-center justify-between border-b border-gray-900/10">
+                <button type="button" onClick={handleDeleteClick} className="text-sm bg-transparent hover:text-red-700 text-red-500 py-2 px-4 mb-4 md:mb-0 md:mr-4">
+                    Deletar projeto
+                </button>
+                <Popup isOpen={isPopupOpen} onClose={handleClosePopup} onConfirm={handleConfirmDelete} />
+                <div className="flex flex-col md:flex-row space-x-1">
+                    <button type="button" className="text-sm text-gray-900 bg-white hover:bg-blue-700 border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:outline-none font-bold py-1 px-2 rounded">
+                        Cancelar
+                    </button>
+                    <button type="submit" className="text-sm bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-1 px-2 rounded">
+                        Salvar
+                    </button>
                 </div>
             </div>
-
             <div >
-                <Comments/>
+                <Comments />
             </div>
         </form>
     )
