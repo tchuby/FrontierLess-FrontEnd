dddcbcf3ea709e04374de3dab7f4f6ea332998a3
LUAN HENRIQUE GROSSERT <luan_grossert@estudante.sc.senai.br>
2024-09-27 20:06:33 -0300

    update Nav

--- src/components/Nav/components/NotificationIcon/index.tsx
+++ src/components/Nav/components/NotificationIcon/index.tsx
@@ -1,7 +1,7 @@
 
 export default function NotificationIcon() {
     return (
-        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
