cedf3cdf8623310af3eea50e0aa725bb5c7f91e0
Luan <luangrossert@gmail.com>
2024-11-07 10:42:32 -0300

    implement flag image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,19 +1,26 @@
 import ImageNext from 'next/image';
+import { useState } from 'react';
 
 interface Props {
-    src?: any
+    src?: string;
 }
 
 export default function Image({ src }: Props) {
+    const [imgSrc, setImgSrc] = useState(src || '/img/flags/brasil.png');
+
+    const handleError = () => {
+        setImgSrc('/img/flags/brasil.png');
+    };
+
     return (
         <div className="relative w-full h-full">
             <ImageNext
                 className="object-cover rounded-t-lg md:rounded-none md:rounded-s-lg"
-                src="/img/brasil.png"
+                src={imgSrc}
                 alt=""
                 layout="fill"
+                onError={handleError}
             />
         </div>
-
-    )
+    );
 }
